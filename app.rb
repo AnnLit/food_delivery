b27f28f40fb4858ead203a164ca79e1778aa67a4
Anna <litfin_k@yahoo.de>
2018-10-16 14:43:04 +0200

    meals

--- app.rb
+++ app.rb
@@ -1,10 +1,10 @@
-# TODO: require relevant files to bootstrap the app.
-# Then you can test your program with:
-#   ruby app.rb
+require_relative 'app/repositories/meal_repository'
+require_relative 'app/controllers/meals_controller'
+require_relative 'router'
+
 csv_file = File.join(__dir__, "data/meals.csv")
 meal_repo = MealRepository.new(csv_file)
 controller = MealsController.new(meal_repo)
+router = Router.new(controller)
 
-controller.list
-controller.create_meal
-controller.list
+router.run
--- router.rb
+++ router.rb
@@ -1,1 +1,42 @@
-# TODO: implement the router of your app.
+class Router
+  def initialize(controller)
+    @controller = controller
+    @running    = true
+  end
+
+  def run
+    puts "Welcome to the Food-Delivery-App!"
+    puts "           --           "
+
+    while @running
+      display_tasks
+      action = gets.chomp.to_i
+      print `clear`
+      route_action(action)
+    end
+  end
+
+  private
+
+  def route_action(action)
+    case action
+    when 1 then @controller.list
+    when 2 then @controller.add
+    when 9 then stop
+    else
+      puts "Please press 1, 2 or 9"
+    end
+  end
+
+  def stop
+    @running = false
+  end
+
+  def display_tasks
+    puts ""
+    puts "What do you want to do next?"
+    puts "1 - List all meals"
+    puts "2 - Create a meal"
+    puts "9 - Stop and exit the program"
+  end
+end# TODO: implement the router of your app.
